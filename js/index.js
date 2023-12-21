38521a4c03a8908b9a02382c9865946dded84a58
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2023-12-21 21:17:17 +0200

    commit2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-const animWidth = 0.4 * window.innerWidth;
-const animHeight = 0.8 * window.innerHeight;
+const anim = document.querySelector('#anim');
 
 function play(){
     const work = document.querySelector('#work');
     work.style.display = 'flex';
-    work.style.width = (animWidth + 10).toString() + 'px';
-    work.style.height = (animHeight + 50).toString() + 'px';
+    work.style.width = (0.4 * window.innerWidth + 10).toString() + 'px';
+    work.style.height = (0.8 * window.innerHeight + 50).toString() + 'px';
 
-    const anim = document.querySelector('#anim');
-    anim.style.height = animHeight.toString() + 'px';
+    anim.style.height = (0.8 * window.innerHeight).toString() + 'px';
 }
 
 const start_reload_btn = document.querySelector('#start-reload-btn');
@@ -23,16 +21,53 @@ function start_reload(){
     circle2.style.display = 'block';
 
     const animBorderWidth = 5;
+    const maxX = anim.clientWidth - circle1.clientWidth;
+    const maxY = anim.clientHeight - circle1.clientHeight;
 
-    circle1.style.transform = 'translate('+(Math.random()*(animWidth-animBorderWidth)-circle1.style.width).toString()+'px,0px)';
-    circle2.style.transform = 'translate(' +
-        Math.random()*(animWidth-circle2.style.width-animBorderWidth) + 'px,' +
-        (animHeight-circle2.clientHeight).toString() + 'px)';
+    let circle1X = Math.random()*maxX;
+    let circle1Y = 0;
+    let circle2X = Math.random()*maxX;
+    let circle2Y = maxY;
+    /*let circle1X = Math.random()*(anim.clientWidth-animBorderWidth)-circle1.style.width;
+    let circle1Y = 0;
+    let circle2X = Math.random()*(anim.clientWidth-circle2.style.width-animBorderWidth);
+    let circle2Y = anim.clientHeight-circle2.clientHeight;*/
+
+    circle1.style.transform = 'translate(' + circle1X + 'px,' + circle1Y + 'px)';
+    circle2.style.transform = 'translate(' + circle2X + 'px,' + circle2Y + 'px)';
+
+    let vel1X = Math.random() * 5;
+    let vel1Y = Math.random() * 5;
+    let vel2X = Math.random() * 5;
+    let vel2Y = Math.random() * 5;
+
+    function move_circles() {
+        circle1X += vel1X;
+        circle1Y += vel1Y;
+        circle2X += vel2X;
+        circle2Y += vel2Y;
+
+        if (circle1X < 0 || circle1X > maxX){
+            vel1X = -vel1X;
+        }
+        if (circle1Y < 0 || circle1Y > maxY){
+            vel1Y = -vel1Y;
+        }
+        if (circle2X < 0 || circle2X > maxX){
+            vel2X = -vel2X;
+        }
+        if (circle2Y < 0 || circle2Y > maxY){
+            vel2Y = -vel2Y;
+        }
+
+        circle1.style.transform = 'translate(' + circle1X + 'px,' + circle1Y + 'px)';
+        circle2.style.transform = 'translate(' + circle2X + 'px,' + circle2Y + 'px)';
+
+        requestAnimationFrame(move_circles);
+    }
 }
 
-function move_circles(){
 
-}
 
 
 
